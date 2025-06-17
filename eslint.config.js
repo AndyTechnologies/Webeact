5dae6303d67419fe60f7fe1a47bcfdd57bfb3a2e
AndyTechnologies <gardyfon2403@gmail.com>
2025-06-17 18:38:05 -0400

    Cambios en las configuraciones de eslint y nuevos paquetes.

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+import js from "@eslint/js";
+import globals from "globals";
+import json from "@eslint/json";
+import markdown from "@eslint/markdown";
+import css from "@eslint/css";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+	{ files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
+	{ files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
+	{ files: ["**/*.{js,mjs,cjs}"], rules: {
+		"eol-last": "error",
+		eqeqeq: ["error", "allow-null"],
+		indent: ["error", "tab", {
+			MemberExpression: "off",
+			SwitchCase: 1,
+		}],
+
+		"no-trailing-spaces": "error",
+
+		"no-unused-vars": ["warn", {
+			vars: "all",
+			args: "none",
+			ignoreRestSiblings: true,
+		}],
+	} },
+	{ files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
+	{ files: ["**/*.jsonc"], plugins: { json }, language: "json/jsonc", extends: ["json/recommended"] },
+	{ files: ["**/*.json5"], plugins: { json }, language: "json/json5", extends: ["json/recommended"] },
+	{ files: ["**/*.md"], plugins: { markdown }, language: "markdown/gfm", extends: ["markdown/recommended"] },
+	{ files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
+]);
